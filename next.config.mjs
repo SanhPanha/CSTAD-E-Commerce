73efecdebcf50f9cb78c73be24428bc918b9fb20
Panha <sanhpanha62@gmail.com>
2024-04-08 12:44:40 +0700

    Build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
     domains: ["store.istad.co", "hips.hearstapps.com"],
+    remotePatterns: [
+      {
+          hostname: "188.166.229.56"
+      },
+      {
+          hostname: "api.istad.co"
+      }
+  ]
   },
+  webpack5: true,
+    webpack: (config) => {
+        config.resolve.fallback = { fs: false };
+
+        return config;
+    }
 };
 
 export default nextConfig;
